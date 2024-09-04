c042b73895b1e9e38a713d6ad5f54019ea917224
NicoGutierrez01 <nicolasgutierrez4245@gmail.com>
2024-09-04 11:51:23 -0300

    Se agrego las bombas y mas pelotas

--- src/entities/WallBrick.js
+++ src/entities/WallBrick.js
@@ -1,23 +1,28 @@
-// grpup of brick
-import { Brick } from "./Brick";
+import Phaser from 'phaser';
+import { Brick } from './Brick'; 
+
 export class WallBrick extends Phaser.GameObjects.Group {
   constructor(scene) {
     super(scene);
-
     this.createWall();
   }
 
   createWall() {
-    for (let i = 0; i < 14; i++) {
-      for (let j = 0; j < 6; j++) {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 4; j++) {
+        let isBallCreator = Phaser.Math.Between(0, 10) > 9;
+        let isBombCreator = Phaser.Math.Between(0, 10) > 9; //configurar la probabilidad
+
         let brick = new Brick(
           this.scene,
-          40 + i * 70,
-          40 + j * 30,
+          40 + i * 100,
+          100 + j * 50,
           60,
           20,
           0xffffff,
-          1
+          1,
+          isBallCreator,
+          isBombCreator
         );
         this.add(brick);
       }
